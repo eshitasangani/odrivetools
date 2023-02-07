60ec77dc5ef6ae7ebaf853dc2ac60be208f603bc
Nivant11 <nivant.desai@gmail.com>
2023-02-07 17:40:29 -0500

    Instantiating odrv object in master process

--- Odrive_Multiprocess.py
+++ Odrive_Multiprocess.py
@@ -10,10 +10,10 @@ SERIAL2 = "206A34A05748"
 SERIAL3 = ""
 
 
-def set_Velocity(vel_current, vel_setpoint, Direction):
+def set_Velocity(vel_current, vel_setpoint, Direction, odrv1):
     vel_ramp = vel_current
     if(Master_drive_enable): #Only continue if the drive is enabled
-        while((odrv1.axis0.input_vel != vel_setpoint) and (odrv1.axis1.input_vel != vel_setpoint) and (odrv2.axis0.input_vel != vel_setpoint) and (odrv2.axis1.input_vel != vel_setpoint) and (odrv3.axis0.input_vel != vel_setpoint) and (odrv3.axis1.input_vel != vel_setpoint)):
+        while((odrv1.axis0.input_vel != vel_setpoint) and (odrv1.axis1.input_vel != vel_setpoint)): #and (odrv2.axis0.input_vel != vel_setpoint) and (odrv2.axis1.input_vel != vel_setpoint) and (odrv3.axis0.input_vel != vel_setpoint) and (odrv3.axis1.input_vel != vel_setpoint)):
             if (Direction = 0): #Forward
                 if  (odrv1.axis0.input_vel < vel_setpoint):
                     odrv1.axis0.input_vel = odrv1.axis0.input_vel + 1
@@ -36,35 +36,37 @@ def set_Velocity(vel_current, vel_setpoint, Direction):
 
 
 def Master_Poll(run_flag, enable_stop, Master_Velocity, Master_drive_enable):
+    print("Finding odrives")
+    odrv1 = odrive.find_any(serial_number = SERIAL1)
+    #odrv2 = odrive.find_any(serial_number = SERIAL2)
+    #odrv3 = odrive.find_any(serial_number = SERIAL3)
     while (run_flag):
         try:
             if keyboard.is_pressed('c'):
                 #Set global stop variable before calibrating, for now just set the input velocity to 0
                 odrv1.axis0.requested_state = AXIS_STATE_FULL_CALIBRATION_SEQUENCE
-                odrv2.axis0.requested_state = AXIS_STATE_FULL_CALIBRATION_SEQUENCE
-                odrv3.axis0.requested_state = AXIS_STATE_FULL_CALIBRATION_SEQUENCE
+                #odrv2.axis0.requested_state = AXIS_STATE_FULL_CALIBRATION_SEQUENCE
+                #odrv3.axis0.requested_state = AXIS_STATE_FULL_CALIBRATION_SEQUENCE
                 odrv1.axis1.requested_state = AXIS_STATE_FULL_CALIBRATION_SEQUENCE
-                odrv2.axis1.requested_state = AXIS_STATE_FULL_CALIBRATION_SEQUENCE
-                odrv3.axis1.requested_state = AXIS_STATE_FULL_CALIBRATION_SEQUENCE
-                while (odrv1.axis0.current_state != AXIS_STATE_IDLE or odrv2.axis0.current_state != AXIS_STATE_IDLE \
-                      or odrv3.axis0.current_state != AXIS_STATE_IDLE or odrv1.axis1.current_state != AXIS_STATE_IDLE or \
-                      odrv2.axis1.current_state != AXIS_STATE_IDLE or odrv3.axis1.current_state != AXIS_STATE_IDLE):
+                #odrv2.axis1.requested_state = AXIS_STATE_FULL_CALIBRATION_SEQUENCE
+                #odrv3.axis1.requested_state = AXIS_STATE_FULL_CALIBRATION_SEQUENCE
+                while (odrv1.axis0.current_state != AXIS_STATE_IDLE or odrv1.axis1.current_state != AXIS_STATE_IDLE):
                           time.sleep(1)
                           print("Calibrating...")
                 
                 #After calibrating, reset the control modes
                 odrv1.axis0.requested_state                = AXIS_STATE_CLOSED_LOOP_CONTROL
                 odrv1.axis0.controller.config.control_mode = CONTROL_MODE_VELOCITY_CONTROL
-                odrv2.axis0.requested_state                = AXIS_STATE_CLOSED_LOOP_CONTROL
-                odrv2.axis0.controller.config.control_mode = CONTROL_MODE_VELOCITY_CONTROL
-                odrv3.axis0.requested_state                = AXIS_STATE_CLOSED_LOOP_CONTROL
-                odrv3.axis0.controller.config.control_mode = CONTROL_MODE_VELOCITY_CONTROL
+                #odrv2.axis0.requested_state                = AXIS_STATE_CLOSED_LOOP_CONTROL
+                #odrv2.axis0.controller.config.control_mode = CONTROL_MODE_VELOCITY_CONTROL
+                #odrv3.axis0.requested_state                = AXIS_STATE_CLOSED_LOOP_CONTROL
+                #odrv3.axis0.controller.config.control_mode = CONTROL_MODE_VELOCITY_CONTROL
                 odrv1.axis1.requested_state                = AXIS_STATE_CLOSED_LOOP_CONTROL
                 odrv1.axis1.controller.config.control_mode = CONTROL_MODE_VELOCITY_CONTROL
-                odrv2.axis1.requested_state                = AXIS_STATE_CLOSED_LOOP_CONTROL
-                odrv2.axis1.controller.config.control_mode = CONTROL_MODE_VELOCITY_CONTROL
-                odrv3.axis1.requested_state                = AXIS_STATE_CLOSED_LOOP_CONTROL
-                odrv3.axis1.controller.config.control_mode = CONTROL_MODE_VELOCITY_CONTROL
+                #odrv2.axis1.requested_state                = AXIS_STATE_CLOSED_LOOP_CONTROL
+                #odrv2.axis1.controller.config.control_mode = CONTROL_MODE_VELOCITY_CONTROL
+                #odrv3.axis1.requested_state                = AXIS_STATE_CLOSED_LOOP_CONTROL
+                #odrv3.axis1.controller.config.control_mode = CONTROL_MODE_VELOCITY_CONTROL
                 print("Closed Loop and Velocity control set for all motors")
 
             if keyboard.is_pressed('s'):
@@ -78,7 +80,7 @@ def Master_Poll(run_flag, enable_stop, Master_Velocity, Master_drive_enable):
                 vel_current         = Master_Velocity
                 Master_Velocity     = vel 
                 Master_direction    = 1
-                set_Velocity(vel_current, Master_Velocity, Master_direction)
+                set_Velocity(vel_current, Master_Velocity, Master_direction, odrv1=odrv1)
 
         except KeyboardInterrupt:
             run_flag = 0
@@ -94,10 +96,7 @@ if __name__ == '__main__':
     Master_drive_enable = Value('i', 0) #When this is zero, the rover should not move no matter what
     Master_direction    = Value('i', 0) #Arbitrary direction for now
 
-
-    print("Finding 3 ODrives")
-    odrv1 = odrive.find_any(serial_number = SERIAL1)
-    odrv2 = odrive.find_any(serial_number = SERIAL2)
-    odrv3 = odrive.find_any(serial_number = SERIAL3)
-   
     p0 = Process(target=Master_Poll, args=(run_flag, enable_stop, Master_Velocity, Master_drive_enable))
+
+    p0.start()
+    p0.join()
